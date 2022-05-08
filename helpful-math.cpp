cf53598387d2a06f7baad5ade7c3ebcc419d2111
Sourav Kumar Saha <94992938+NULLcodehub@users.noreply.github.com>
2022-05-09 03:52:16 +0600

    new commit

---
+++ helpful-math.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a[100];
+    int c=0;
+    string s="";
+    cin>>s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == '+'){
+            continue;
+        }else{
+            a[c]=s[i]-'0';
+            c++;
+        }
+    }
+
+    sort(a,a+c);
+    
+    for (int i = 0; i <c; i++)
+    {
+        cout<<a[i];
+        if(i== c-1){
+            break;
+        }
+        cout<<"+";
+    }
+    
+    return 0;
+}
